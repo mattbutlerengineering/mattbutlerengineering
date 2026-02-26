750bbfd64f37bacbf494e38d73f3099c37ab5b94
Matt Butler <mattwbutler@gmail.com>
2026-02-25 20:48:03 -0800

    feat(config): upgrade to ESLint 10, add jsx-a11y and react-refresh plugins
    - Upgrade @eslint/js and eslint to ^10.0.1
    - Upgrade typescript to ^5.9.3 and typescript-eslint to ^8.56.1
    - Upgrade eslint-plugin-react-hooks to ^7.0.1
    - Add eslint-plugin-jsx-a11y for accessibility linting
    - Add eslint-plugin-react-refresh for fast refresh validation
    - Add @eslint/compat to wrap plugins not yet supporting ESLint 10 API

--- packages/config/eslint/react.js
+++ packages/config/eslint/react.js
@@ -1,13 +1,18 @@
+import { fixupPluginRules } from "@eslint/compat";
 import baseConfig from "./base.js";
 import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   ...baseConfig,
   {
     plugins: {
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
+      react: fixupPluginRules(reactPlugin),
+      "react-hooks": fixupPluginRules(reactHooksPlugin),
+      "jsx-a11y": fixupPluginRules(jsxA11y),
+      "react-refresh": reactRefresh,
     },
     settings: {
       react: {
@@ -17,8 +22,13 @@ export default [
     rules: {
       ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
